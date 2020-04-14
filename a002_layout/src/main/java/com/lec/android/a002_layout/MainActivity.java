81b01aae749dc6f219c5a1ded06d189ecdd6538f
Jay9011 <whdtjq5904@gmail.com>
2020-04-14 13:51:32 +0900

    Listener 시작

--- a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
+++ a002_layout/src/main/java/com/lec/android/a002_layout/MainActivity.java
@@ -17,26 +17,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear1);
+        setContentView(R.layout.constraint1);
 
         int ss = 100;
         String tt = "하핫! (코쓱)";
         char ch = 'a';
         short s = 200;
-
-        Button1 = (ToggleButton) findViewById(R.id.visibleButton);
-        TargetText = (TextView) findViewById(R.id.textView3);
-
-
-        Button1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    TargetText.setVisibility(View.VISIBLE);
-                }else{
-                    TargetText.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
     }
 }
